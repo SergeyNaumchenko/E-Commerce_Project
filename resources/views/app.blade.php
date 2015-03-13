7dd1e7584eaaa4040c9c7dab93bb9431179aaa7c
sergey <sergey89n@gmail.com>
2015-03-12 23:57:04 -0400

    added search bar.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,8 +7,10 @@
 	<title>Laravel</title>
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('/css/search.css') }}" rel="stylesheet">
 
-	<!-- Fonts -->
+
+    <!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -55,9 +57,13 @@
 
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
-						<li><a href="{{ url('/auth/login') }}"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                            <form class="webdesigntuts-workshop" action="" method="">
+                                <input type="search" placeholder="Search...">
+                                <button>Search</button>
+                            </form>
+                        <li><a href="{{ url('/auth/login') }}"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
 						<li><a href="{{ url('/auth/register') }}"><span class="glyphicon glyphicon-user"></span> Sign up</a></li>
-					@else
+                    @else
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,24 +16,24 @@
             <img src="img/firstSlide.jpg" alt="First slide">
             <div class="container">
                 <div class="carousel-caption">
-                    <h1>Example headline.</h1>
-                    <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
-                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+                    <h1>Ultimate Home Audio Event.</h1>
+                    <p>Save on premium sound for audiophiles and enthusiasts.</p>
+                    <p><a class="btn btn-lg btn-primary" href="http://localhost:8000/auth/register" role="button">Sign up today</a></p>
                 </div>
             </div>
         </div>
         <div class="item">
-            <img src="data:image/gif;base64,R0lGODlhAQABAIAAAGZmZgAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
+            <img src="img/secondSlide.jpg" alt="Second slide">
             <div class="container">
                 <div class="carousel-caption">
-                    <h1>Another example headline.</h1>
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    <h1>March event 2015</h1>
+                    <p>Experience the keynote</p>
                     <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
                 </div>
             </div>
         </div>
         <div class="item">
-            <img src="data:image/gif;base64,R0lGODlhAQABAIAAAFVVVQAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+            <img src="img/thirdSlide.jpg" alt="Third slide">
             <div class="container">
                 <div class="carousel-caption">
                     <h1>One more for good measure.</h1>

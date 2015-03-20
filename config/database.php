395ad81d458e74a0fee358a43970307fb60c9afd
sergey <sergey89n@gmail.com>
2015-03-19 21:38:49 -0400

    Configured mail.php file to send password reset link by email. Also switched from sqlite to mysql database.

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
 	|
 	*/
 
-	'default' => 'sqlite',
+	'default' => 'mysql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -54,9 +54,9 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
+			'host'      => env('DB_HOST', '127.0.0.1'),
+			'database'  => env('DB_DATABASE', 'LaravelApp'),
+			'username'  => env('DB_USERNAME', 'root'),
 			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',

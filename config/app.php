ea245c80914be0aa34ca195db98a972c4daf8de1
sergey <sergey89n@gmail.com>
2015-04-12 22:06:06 -0400

    Added new package to composer.json

--- config/app.php
+++ config/app.php
@@ -137,6 +137,7 @@ return [
 		'Illuminate\View\ViewServiceProvider',
         'Illuminate\Html\HtmlServiceProvider',
         'Intervention\Image\ImageServiceProvider',
+        'Gloudemans\Shoppingcart\ShoppingcartServiceProvider',
 
 
 		/*
@@ -197,8 +198,8 @@ return [
 		'View'      => 'Illuminate\Support\Facades\View',
         'Form'      => 'Illuminate\Html\FormFacade',
         'HTML'      => 'Illuminate\Html\HtmlFacade',
-        'Image'     => 'Intervention\Image\Facades\Image'
-
+        'Image'     => 'Intervention\Image\Facades\Image',
+        'Cart'      => 'Gloudemans\Shoppingcart\Facades\Cart'
     ],
 
 ];

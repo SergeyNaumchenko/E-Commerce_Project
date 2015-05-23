447eb80a95c9b60150832e876816bd65a1c1b6c1
sergey <sergey89n@gmail.com>
2015-05-23 04:13:49 -0400

    Added error page for saved carts view.

---
+++ resources/views/store/sections/no_saved_carts.blade.php
@@ -0,0 +1,10 @@
+<div class="jumbotron">
+    <h1>You have no saved carts!</h1>
+    <p>
+        If you are trying to save cart to your wish list and it remains empty, you may have cookies disabled
+        in your browser settings. If that's the case, please enable acceptance of cookies and try again.
+    </p>
+    <p>
+        {!! HTML::link(route('store.index'), 'Continue Shopping', array('class'=>'btn btn-primary btn-lg')) !!}
+    </p>
+</div>
